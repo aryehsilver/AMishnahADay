03be9b60f4472727e1b79a1ca728ac9f58128161
Aryeh Silver <aryehsilver@hotmail.com>
2021-05-14 00:55:17 +0100

    Add settings model

---
+++ AMishnahADay/Migrations/20210513235350_AddSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AMishnahADay.Migrations
+{
+    public partial class AddSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MishnahID = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartOnSystemStartup = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TimeForToast = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Settings_Mishnayos_MishnahID",
+                        column: x => x.MishnahID,
+                        principalTable: "Mishnayos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Settings_MishnahID",
+                table: "Settings",
+                column: "MishnahID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
