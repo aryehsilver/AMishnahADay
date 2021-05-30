44d6d19cb67646799004268bf58afbbb4f9dfd31
Aryeh Silver <aryehsilver@hotmail.com>
2021-05-30 01:37:50 +0100

    Tidy up Notify icon VM

--- AMishnahADay/ViewModels/NotifyIconViewModel.cs
+++ AMishnahADay/ViewModels/NotifyIconViewModel.cs
@@ -4,47 +4,21 @@ using System.Windows;
 using System.Windows.Input;
 
 namespace AMishnahADay.ViewModels {
-  /// <summary>
-  /// Provides bindable properties and commands for the NotifyIcon. In this sample, the
-  /// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
-  /// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
-  /// </summary>
   public class NotifyIconViewModel {
-    /// <summary>
-    /// Shows a window, if none is already open.
-    /// </summary>
-    public ICommand ShowWindowCommand =>
+    public ICommand ShowSettingsWindowCommand =>
       new DelegateCommand {
-        CanExecuteFunc = () => Application.Current.MainWindow == null,
-        CommandAction = () => {
-          //Settings settings = new();
-          //if (settings.Success) {
-          //  settings.Show();
-          //}
-          new MainWindow().ShowDialog();
-        }
+        CommandAction = () => new Settings().ShowDialog()
       };
 
-    /// <summary>
-    /// Hides the main window. This command is only enabled if a window is open.
-    /// </summary>
-    public ICommand HideWindowCommand =>
+    public ICommand OpenMainWindowCommand =>
       new DelegateCommand {
-        CommandAction = () => Application.Current.MainWindow.Close(),
-        CanExecuteFunc = () => Application.Current.MainWindow != null
+        CommandAction = () => new MainWindow().ShowDialog()
       };
 
-    /// <summary>
-    /// Shuts down the application.
-    /// </summary>
     public ICommand ExitApplicationCommand =>
       new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
   }
 
-
-  /// <summary>
-  /// Simplistic delegate command for the demo.
-  /// </summary>
   public class DelegateCommand : ICommand {
     public Action CommandAction { get; set; }
     public Func<bool> CanExecuteFunc { get; set; }
