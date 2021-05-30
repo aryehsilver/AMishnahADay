57254a0ddd71164dbd09bd954d958c3939e7540f
Aryeh Silver <aryehsilver@hotmail.com>
2021-05-30 01:39:44 +0100

    Tidy up and added merged dictionaries for themes

--- AMishnahADay/App.xaml.cs
+++ AMishnahADay/App.xaml.cs
@@ -1,13 +1,14 @@
-﻿using AMishnahADay.Views;
+﻿using AMishnahADay.Models;
+using AMishnahADay.Views;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Toolkit.Uwp.Notifications;
 using NotifyIcon;
 using System;
 using System.IO;
+using System.Linq;
 using System.Timers;
 using System.Windows;
 using Telerik.Windows.Controls;
-using Telerik.Windows.Controls.MaterialControls;
-using Windows.Foundation.Collections;
 
 namespace AMishnahADay {
   /// <summary>
@@ -15,14 +16,11 @@ namespace AMishnahADay {
   /// </summary>
   public partial class App : Application {
     #region Props, Fields & consts
-    private const string APP_ID = "A Mishnah A Day";
     private static readonly string App_Folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\AMAD_App";
-    private static readonly string Startup_Folder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
     private TaskbarIcon notifyIcon;
     private Timer timer;
     public DateTime LastRead { get; set; }
     public bool RunsOnStartup { get; set; } = true;
-    public bool SplashScreen { get; set; } = true;
     public double Interval { get; set; } = 30;
     public DateTime TimeForToast { get; set; }
     public int MishnahNumber { get; set; }
@@ -33,22 +31,10 @@ namespace AMishnahADay {
 
     protected override void OnStartup(StartupEventArgs e) {
       // Listen to notification activation
-      ToastNotificationManagerCompat.OnActivated += toastArgs => {
-        // Obtain the arguments from the notification
-        ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
-
-        // Obtain any user input (text boxes, menu selections) from the notification
-        ValueSet userInput = toastArgs.UserInput;
-
-        // Need to dispatch to UI thread if performing UI operations
+      ToastNotificationManagerCompat.OnActivated += toastArgs =>
         Current.Dispatcher.Invoke(delegate {
-          // TODO: Show the corresponding content
-          MessageBox.Show("Toast activated. Args: " + toastArgs.Argument);
-
-          // TODO: In this window show the Mishnah
           new MainWindow().ShowDialog();
         });
-      };
 
       if (!IsStarted) {
         base.OnStartup(e);
@@ -56,62 +42,32 @@ namespace AMishnahADay {
         notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
 
         StyleManager.ApplicationTheme = new FluentTheme();
-        FluentPalette.LoadPreset(FluentPalette.ColorVariation.Dark);
-        ThemeEffectsHelper.IsAcrylicEnabled = false;
+        SetThemeMode(new AppDbContext());
+        //ThemeEffectsHelper.IsAcrylicEnabled = false;
 
         Start();
+        //PopTheToast();
       }
+
+      using AppDbContext context = new();
+      context.Database.Migrate();
     }
+
+    private void SetThemeMode(AppDbContext context) =>
+      FluentPalette.LoadPreset(context.Settings.SingleOrDefault().DarkMode ? FluentPalette.ColorVariation.Dark : FluentPalette.ColorVariation.Dark);
 
     public void Start() {
       if (!Directory.Exists(App_Folder)) {
         Directory.CreateDirectory(App_Folder);
       }
 
-      TimeForToast = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 15, 0, 0);
+      DateTime timeForToast = new AppDbContext().Settings.SingleOrDefault().TimeForToast;
+      TimeForToast = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, timeForToast.Hour, timeForToast.Minute, 0);
 
-      //ReadFromXml();
       //RunTimer();
 
       IsStarted = true;
     }
-
-    //private void ReadFromXml() {
-    //  bool success;
-    //  Exception exception = new();
-
-    //  try {
-    //    if (File.Exists(Path.Combine(App_Folder, "Settings.xml"))) {
-    //      System.Xml.XmlDocument readFile = new();
-    //      readFile.Load(Path.Combine(App_Folder, "Settings.xml"));
-
-    //      System.Xml.XmlNode startupNode = readFile.SelectSingleNode("/Settings/Startup");
-    //      RunsOnStartup = startupNode.InnerText == "True";
-
-    //      System.Xml.XmlNode intervalNode = readFile.SelectSingleNode("/Settings/Interval");
-    //      Interval = double.TryParse(intervalNode.InnerText, out double outInterval) ? outInterval : 30;
-
-    //      System.Xml.XmlNode notifTextNode = readFile.SelectSingleNode("/Settings/NotifText");
-    //    } else {
-    //      RunsOnStartup = true;
-    //      SplashScreen = true;
-    //    }
-
-    //    LastRead = DateTime.Now;
-
-    //    success = true;
-    //  } catch (Exception ex) {
-    //    success = false;
-    //    exception = ex;
-    //  }
-
-    //  if (!success) {
-    //    RadWindow.Alert(new DialogParameters {
-    //      Header = "A Mishnah A Day App - Error",
-    //      Content = "Error reading the settings" + Environment.NewLine + exception.Message
-    //    });
-    //  }
-    //}
 
     private void RunTimer() {
       timer = new Timer {
@@ -122,26 +78,20 @@ namespace AMishnahADay {
       timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
     }
 
-    private void Timer_Elapsed(object sender, ElapsedEventArgs e) {
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e) =>
+      // TODO: Use Ninject Messaging to update timer...
+
       // ISSUE: This will only get hit the next time the timer finishes it's elapsed time.
       // What if the user has chosen a smaller interval, say from 30 min down to 5,
       // how will the app know to change it until another 30 min passes and we hit the elapsed?
       // Settings need to raise an event which will be picked up here...
-      CheckTimeStamps();
+
+      // CheckTimeStamps();
       PopTheToast();
-    }
 
-    private void CheckTimeStamps() {
-      DateTime time = File.GetLastWriteTime(Path.Combine(App_Folder, "Settings.xml"));
-      if (LastRead < time) {
-        //ReadFromXml();
-
-        LastRead = DateTime.Now;
-      }
-    }
-
-    public static void PopTheToast() =>
+    public void PopTheToast() =>
       new ToastContentBuilder()
+          .SetToastScenario(ToastScenario.Reminder)
           .AddArgument("masechtah", "berachot")
           .AddArgument("mishnah", 1)
           .AddArgument("perek", 1)
@@ -150,8 +100,20 @@ namespace AMishnahADay {
           //.AddHeroImage(new Uri("file:///"))
           .AddAppLogoOverride(new Uri("file:///" + Path.GetFullPath("Data/AMishnahADay.png")), ToastGenericAppLogoCrop.Circle)
           .AddAttributionText("Via AMAD")
-          .Schedule(/*TimeForToast*/ DateTime.Now.AddSeconds(10) /*, toast => toast.ExpirationTime = DateTime.Now.AddMinutes(2)*/);
-          //.Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(2));
+          .AddToastInput(new ToastSelectionBox("snoozeTime") {
+            DefaultSelectionBoxItemId = "15",
+            Items = {
+              new ToastSelectionBoxItem("5", "5 minutes"),
+              new ToastSelectionBoxItem("15", "15 minutes"),
+              new ToastSelectionBoxItem("60", "1 hour"),
+              new ToastSelectionBoxItem("240", "4 hours"),
+              new ToastSelectionBoxItem("1440", "1 day")
+            }
+          })
+          .AddButton(new ToastButtonSnooze() { SelectionBoxId = "snoozeTime" })
+          .AddButton(new ToastButtonDismiss())
+          // TODO: If time has passed an exception will be thrown, need to use tommorow in that case...
+          .Schedule(TimeForToast, toast => toast.ExpirationTime = TimeForToast.AddMinutes(1));
 
     protected override void OnExit(ExitEventArgs e) {
       // The icon would clean up automatically, but this is cleaner
