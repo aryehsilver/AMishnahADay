e015aba6489616f1ce25d693ad5726a6a3395342
Aryeh Silver <aryehsilver@hotmail.com>
2023-09-14 20:52:03 +0100

    Update to .Net 6

--- AMishnahADay/ViewModels/NotifyIconViewModel.cs
+++ AMishnahADay/ViewModels/NotifyIconViewModel.cs
@@ -1,43 +1,40 @@
 ﻿using AMishnahADay.Views;
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System.Windows;
 
-namespace AMishnahADay.ViewModels {
-  public class NotifyIconViewModel : ViewModelBase {
-    public NotifyIconViewModel() =>
-      LoadCommands();
+namespace AMishnahADay.ViewModels;
 
-    private void LoadCommands() {
-      ShowSettingsWindowCommand = new RelayCommand(ShowSettingsWindowCommandExecute);
-      OpenMainWindowCommand = new RelayCommand(OpenMainWindowCommandExecute);
-      ExitApplicationCommand = new RelayCommand(ExitApplicationCommandExecute);
-    }
+public class NotifyIconViewModel : ViewModelBase {
+  public NotifyIconViewModel() =>
+    LoadCommands();
 
-
-    #region ShowSettingsWindowCommand
-
-    public RelayCommand ShowSettingsWindowCommand { get; set; }
-
-    private void ShowSettingsWindowCommandExecute() =>
-      new Settings().ShowDialog();
-
-    #endregion
-
-    #region OpenMainWindowCommand
-    public RelayCommand OpenMainWindowCommand { get; set; }
-
-    private void OpenMainWindowCommandExecute() =>
-      new MainWindow().ShowDialog();
-
-    #endregion
-
-    #region ExitApplicationCommand
-    public RelayCommand ExitApplicationCommand { get; set; }
-
-    private void ExitApplicationCommandExecute() =>
-      Application.Current.Shutdown();
-
-    #endregion
+  private void LoadCommands() {
+    ShowSettingsWindowCommand = new RelayCommand(ShowSettingsWindowCommandExecute);
+    OpenMainWindowCommand = new RelayCommand(OpenMainWindowCommandExecute);
+    ExitApplicationCommand = new RelayCommand(ExitApplicationCommandExecute);
   }
+
+  #region ShowSettingsWindowCommand
+
+  public RelayCommand ShowSettingsWindowCommand { get; set; }
+
+  private void ShowSettingsWindowCommandExecute() =>
+    new Settings().ShowDialog();
+
+  #endregion
+
+  #region OpenMainWindowCommand
+  public RelayCommand OpenMainWindowCommand { get; set; }
+
+  private void OpenMainWindowCommandExecute() =>
+    new MainWindow().ShowDialog();
+
+  #endregion
+
+  #region ExitApplicationCommand
+  public RelayCommand ExitApplicationCommand { get; set; }
+
+  private void ExitApplicationCommandExecute() =>
+    System.Windows.Application.Current.Shutdown();
+
+  #endregion
 }
