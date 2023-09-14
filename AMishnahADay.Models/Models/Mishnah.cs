c9c608d1f95971ffe38b5bb2661e76379f94c104
Aryeh Silver <aryehsilver@hotmail.com>
2023-09-14 20:42:31 +0100

    Update to .Net 6

--- AMishnahADay.Models/Models/Mishnah.cs
+++ AMishnahADay.Models/Models/Mishnah.cs
@@ -1,12 +1,12 @@
-﻿namespace AMishnahADay.Models.Models {
-  public class Mishnah {
-    public int ID { get; set; }
-    public int MishnahNumber { get; set; }
-    public string HebrewText { get; set; }
-    public string EnglishText { get; set; }
-    public int PerekID { get; set; }
-    public Perek Perek { get; set; }
-    public int MasechtahID { get; set; }
-    public Masechtah Masechtah { get; set; }
-  }
+﻿namespace AMishnahADay.Models.Models;
+
+public class Mishnah {
+  public int ID { get; set; }
+  public int MishnahNumber { get; set; }
+  public string HebrewText { get; set; }
+  public string EnglishText { get; set; }
+  public int PerekID { get; set; }
+  public Perek Perek { get; set; }
+  public int MasechtahID { get; set; }
+  public Masechtah Masechtah { get; set; }
 }
